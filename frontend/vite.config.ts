ee99516405cf119828bf2067aa772c057c09349f
elizavetarybina <liza.rybina@inbox.ru>
2025-11-15 16:11:47 +0300

    setup tests and aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@entities': path.resolve(__dirname, './src/entities'),
+			'@widgets': path.resolve(__dirname, './src/widgets'),
+			'@app': path.resolve(__dirname, './src/app'),
+			'@shared': path.resolve(__dirname, './src/shared')
+		}
+	}
 })
