3ee2cd236062b93433fb94b3a3c20cc084d9fc95
elizavetarybina <liza.rybina@inbox.ru>
2025-11-16 19:08:50 +0300

    added a hotkey for searching

--- frontend/src/components/MainPage/SearchBar/component.tsx
+++ frontend/src/components/MainPage/SearchBar/component.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import cls from './style.module.scss'
 
 interface SearchBarProps {
@@ -8,10 +8,32 @@ interface SearchBarProps {
 
 export const SearchBar: React.FC<SearchBarProps> = React.memo(
 	({ value, onChange }) => {
+		const inputRef = useRef<HTMLInputElement>(null)
+
+		useEffect(() => {
+			const handleKeydown = (e: KeyboardEvent) => {
+				if (
+					e.key === '/' &&
+					!['INPUT', 'TEXTAREA'].includes((e.target as HTMLElement).tagName) &&
+					!e.ctrlKey &&
+					!e.metaKey &&
+					!e.altKey &&
+					!e.shiftKey
+				) {
+					e.preventDefault()
+					inputRef.current?.focus()
+				}
+			}
+
+			window.addEventListener('keydown', handleKeydown)
+			return () => window.removeEventListener('keydown', handleKeydown)
+		}, [])
+
 		return (
 			<fieldset className={cls.field}>
 				<legend className={cls.legend}>Поиск</legend>
 				<input
+					ref={inputRef}
 					type='search'
 					placeholder='Поиск по названию...'
 					value={value}
